c2135b8e662faf363a6089c8b5bfb92cba7463f2
Cintia Araujo <cintia_araujo@brown.edu>
2019-05-01 17:34:51 -0400

    changing pitch incrementally

--- hi.py
+++ hi.py
@@ -1,46 +1,119 @@
+# import pyaudio
+# import wave
+# import sys
+# import numpy as np
+# from subprocess import call
+
+# def getNewWave(i, data):
+#     newdata = np.frombuffer(data, np.int8)
+#     # scaled = np.int8(newdata/np.max(np.abs(newdata)) * 500)
+#     # volume = float(i/100)  
+#     # f = -(float(i) * 10) + 700
+#     # newdata = np.fromstring(newdata, np.int8) / 10 * 5
+#     # scaled = np.int16(newdata/np.max(np.abs(newdata)) * 500)
+#     # newdata = (newdata * 0.95).astype(np.int8)
+#     return newdata
+
+# def main():
+
+#     CHUNK = 2**16
+
+#     n = 7
+
+#     if len(sys.argv) < 2:
+#         print("Missing input wav file. File must also have single channel")
+#         sys.exit(-1)
+
+#     wf = wave.open(sys.argv[1], 'rb')
+#     p = pyaudio.PyAudio()
+
+#     stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+#                 channels=wf.getnchannels(),
+#                 rate=wf.getframerate(),
+#                 output=True)
+
+#     data = wf.readframes(CHUNK)
+#     i = 0.0
+#     while data != '':
+#         # stream.write(getNewWave(i, data))
+#         # i = i + 0.1
+#         # print(i)
+#         data = np.frombuffer(data, np.int16)
+#         data = np.fft.rfft(data)
+   
+#         # This does the shifting
+#         data2 = [0]*len(data)
+#         if n >= 0:
+#             data2[n:len(data)] = data[0:(len(data)-n)]
+#             data2[0:n] = data[(len(data)-n):len(data)]
+#         else:
+#             data2[0:(len(data)+n)] = data[-n:len(data)]
+#             data2[(len(data)+n):len(data)] = data[0:-n]
+   
+#         data = np.array(data2)
+#         # Done shifting
+   
+#         # inverse transform to get back to temporal data
+#         data = np.fft.irfft(data)
+   
+#         dataout = np.array(data, dtype='int16')
+#         chunkout = wave.struct.pack("%dh"%(len(dataout)), *list(dataout))
+#         stream.write(chunkout)
+#         data = wf.readframes(CHUNK)
+
+#     stream.stop_stream()
+#     stream.close()
+#     p.terminate()
+
+# main()
+
 import pyaudio
-import wave
-import sys
+import sys, time
 import numpy as np
+import wave
 
-def getNewWave(i, data):
+CHUNK = 2**18
 
-    newdata = np.frombuffer(data, np.int8)
-    print(type(newdata))
-    # scaled = np.int8(newdata/np.max(np.abs(newdata)) * 500)
-    # volume = float(i/100)  
-    # f = -(float(i) * 10) + 700
-    # newdata = np.fromstring(newdata, np.int8) / 10 * 5
-    # scaled = np.int16(newdata/np.max(np.abs(newdata)) * 500)
-    # newdata = (newdata * 0.95).astype(np.int8)
-    return newdata
+if len(sys.argv) < 2:
+    print("Missing input wav file. File must also have single channel")
+    sys.exit(-1)
 
-def main():
+wf = wave.open(sys.argv[1], 'rb')
+p = pyaudio.PyAudio()
 
-    CHUNK = 1024
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+            channels=wf.getnchannels(),
+            rate=wf.getframerate(),
+            output=True)
 
-    if len(sys.argv) < 2:
-        print("Missing input wav file. File must have single channel")
-        sys.exit(-1)
+n = 500
 
-    wf = wave.open(sys.argv[1], 'rb')
-    p = pyaudio.PyAudio()
+data = wf.readframes(CHUNK)
+while data != '':
+    n = n - 100
+    print(n)
+    data = np.frombuffer(data, np.int16)
+    data = np.array(wave.struct.unpack("%dh"%(len(data)), data))
+    data = np.fft.rfft(data)
+    data2 = [0]*len(data)
+    if n >= 0:
+        data2[n:len(data)] = data[0:(len(data)-n)]
+        data2[0:n] = data[(len(data)-n):len(data)]
+    else:
+        data2[0:(len(data)+n)] = data[-n:len(data)]
+        data2[(len(data)+n):len(data)] = data[0:-n]
+   
+    data = np.array(data2)
+    data = np.fft.irfft(data)
+   
+    dataout = np.array(data, dtype='int16')
+    output = wave.struct.pack("%dh"%(len(dataout)), *list(dataout)) #convert back to 16-bit data
 
-    stream = p.open(format=pyaudio.paInt16,
-                    channels=1,
-                    rate=wf.getframerate(),
-                    output=True, frames_per_buffer=CHUNK)
+    
 
+    stream.write(output)
     data = wf.readframes(CHUNK)
-    i = 0
-    while data != '':
-        stream.write(getNewWave(i, data))
-        # placeholder
-        i = i + 5
-        data = wf.readframes(CHUNK)
-
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-main()
+ 
+stream.stop_stream()
+stream.close()
+p.terminate()
