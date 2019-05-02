5485a3a02c30d075e8199d1ba348b1a6e8459759
Cintia Araujo <cintia_araujo@brown.edu>
2019-05-02 09:59:18 -0400

    added volume properties

--- hi.py
+++ hi.py
@@ -1,81 +1,13 @@
-# import pyaudio
-# import wave
-# import sys
-# import numpy as np
-# from subprocess import call
-
-# def getNewWave(i, data):
-#     newdata = np.frombuffer(data, np.int8)
-#     # scaled = np.int8(newdata/np.max(np.abs(newdata)) * 500)
-#     # volume = float(i/100)  
-#     # f = -(float(i) * 10) + 700
-#     # newdata = np.fromstring(newdata, np.int8) / 10 * 5
-#     # scaled = np.int16(newdata/np.max(np.abs(newdata)) * 500)
-#     # newdata = (newdata * 0.95).astype(np.int8)
-#     return newdata
-
-# def main():
-
-#     CHUNK = 2**16
-
-#     n = 7
-
-#     if len(sys.argv) < 2:
-#         print("Missing input wav file. File must also have single channel")
-#         sys.exit(-1)
-
-#     wf = wave.open(sys.argv[1], 'rb')
-#     p = pyaudio.PyAudio()
-
-#     stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-#                 channels=wf.getnchannels(),
-#                 rate=wf.getframerate(),
-#                 output=True)
-
-#     data = wf.readframes(CHUNK)
-#     i = 0.0
-#     while data != '':
-#         # stream.write(getNewWave(i, data))
-#         # i = i + 0.1
-#         # print(i)
-#         data = np.frombuffer(data, np.int16)
-#         data = np.fft.rfft(data)
-   
-#         # This does the shifting
-#         data2 = [0]*len(data)
-#         if n >= 0:
-#             data2[n:len(data)] = data[0:(len(data)-n)]
-#             data2[0:n] = data[(len(data)-n):len(data)]
-#         else:
-#             data2[0:(len(data)+n)] = data[-n:len(data)]
-#             data2[(len(data)+n):len(data)] = data[0:-n]
-   
-#         data = np.array(data2)
-#         # Done shifting
-   
-#         # inverse transform to get back to temporal data
-#         data = np.fft.irfft(data)
-   
-#         dataout = np.array(data, dtype='int16')
-#         chunkout = wave.struct.pack("%dh"%(len(dataout)), *list(dataout))
-#         stream.write(chunkout)
-#         data = wf.readframes(CHUNK)
-
-#     stream.stop_stream()
-#     stream.close()
-#     p.terminate()
-
-# main()
-
 import pyaudio
-import sys, time
+import sys
 import numpy as np
 import wave
+import audioop
 
-CHUNK = 2**18
+CHUNK = 2**15
 
 if len(sys.argv) < 2:
-    print("Missing input wav file. File must also have single channel")
+    print("Missing input wav file")
     sys.exit(-1)
 
 wf = wave.open(sys.argv[1], 'rb')
@@ -86,33 +18,42 @@ stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
             rate=wf.getframerate(),
             output=True)
 
-n = 500
+n = 1000
+
+#range 1000 to -200
 
 data = wf.readframes(CHUNK)
+i = 0
+# n and i will later be Y values of the two hand positions. Need to set range for both
 while data != '':
-    n = n - 100
+    n = n - 50
     print(n)
     data = np.frombuffer(data, np.int16)
+    ################################## Code to change pitch
     data = np.array(wave.struct.unpack("%dh"%(len(data)), data))
-    data = np.fft.rfft(data)
-    data2 = [0]*len(data)
-    if n >= 0:
-        data2[n:len(data)] = data[0:(len(data)-n)]
-        data2[0:n] = data[(len(data)-n):len(data)]
-    else:
-        data2[0:(len(data)+n)] = data[-n:len(data)]
-        data2[(len(data)+n):len(data)] = data[0:-n]
+    if len(data) is not 0:
+        data = np.fft.rfft(data)
+        data2 = [0]*len(data)
+        if n >= 0:
+            data2[n:len(data)] = data[0:(len(data)-n)]
+            data2[0:n] = data[(len(data)-n):len(data)]
+        else:
+            data2[0:(len(data)+n)] = data[-n:len(data)]
+            data2[(len(data)+n):len(data)] = data[0:-n]
    
-    data = np.array(data2)
-    data = np.fft.irfft(data)
+        data = np.array(data2)
+        data = np.fft.irfft(data)
    
-    dataout = np.array(data, dtype='int16')
-    output = wave.struct.pack("%dh"%(len(dataout)), *list(dataout)) #convert back to 16-bit data
+        dataout = np.array(data, dtype='int16')
+        output = wave.struct.pack("%dh"%(len(dataout)), *list(dataout)) 
 
-    
+    ################################ Code to change volume
+        newdata = audioop.mul(data, 2, i)
+        i = i + 0.1
+    #################################
 
-    stream.write(output)
-    data = wf.readframes(CHUNK)
+        stream.write(newdata)
+        data = wf.readframes(CHUNK)
  
 stream.stop_stream()
 stream.close()
